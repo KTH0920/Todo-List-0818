fbd3dbe931de8893ef3d37f3dbdc671e4e6d4d48
KTH0920 <taehyun9482@gmail.com>
2025-08-20 10:48:35 +0900

    update

--- frontend/src/components/TodoList.jsx
+++ frontend/src/components/TodoList.jsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
 import "./TodoList.css";
 import TodoItem from "./TodoItem";
 
-const TodoList = ({ todos, onDelete }) => {
+const TodoList = ({ todos, onUpdateChecked, onUpdateText }) => {
+  const [q, setQ] = useState("");
+  const filtered = useMemo(() => {
+    const kw = q.trim().toLowerCase();
+
+    if (!kw) return todos;
+    return todos.filter((t) => (t.text ?? "").toLowerCase().includes(kw));
+  }, [todos, q]);
   return (
     <div className="TodoList">
       <h4>Todo List 🌱</h4>
-      <input type="text" placeholder="검색어를 입력하세요." />
-      <div className="todos-wrapper">
-        {todos.map((todo, i) => (
-          <TodoItem key = {i} todo = {todo} onDelete={onDelete}/>
+      <input
+        type="text"
+        value={q}
+        onChange={(e) => setQ(e.target.value)}
+        placeholder="검색어를 입력하세요."
+      />
+      <div className="todos_wrapper">
+        {filtered.map((todo) => (
+          <TodoItem
+            key={todo._id}
+            todo={todo}
+            onUpdateChecked={onUpdateChecked}
+            onUpdateText={onUpdateText}
+          />
         ))}
       </div>
     </div>
